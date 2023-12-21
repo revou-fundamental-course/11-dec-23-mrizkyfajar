86f82bdd03a95662a6582306abeb3892e861577a
unknown <rizkyfajar899@gmail.com>
2023-12-21 22:53:05 +0700

    first push

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Website Pendidikan | Selamat Datang di Fajar Coding</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- header -->
+    <div class="header">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><strong>Belajar Coding, Ya di FAJAR CODING!!!  </strong></a>
+        </div>
+    </div>
+    <header>
+        <div class="container">
+            <h1><strong style="font-family:Grocery Rounded;">Fajar Coding</strong></h1>
+            <ul>
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="our_profile.php">Our Profile</a></li>
+                <li><a href="achievement.php">Achievement</a></li>
+                <li><a href="messageus.php">Message Us</a></li>
+                <li><a href="account.php">Account</a></li>
+            </ul>
+        </div>
+    </header>
+    
+    <!-- banner -->
+    <section class="banner">
+        <h1>Halo Harfi, Selamat  Datang  di<strong>  FAJAR CODING</strong></h1>
+    </section>
+
+    <!-- fasilitas -->
+    <div class="fasilitas">
+        <div class="container">
+            <h3>Fasilitas Kami</h3>
+            <p>Berikut ini fasilitas dari kami:</p>
+            <div class="box">
+                <div class="col-4">
+                    <div class="iconBulat"><i class="fa-solid fa-cart-flatbed"></i></div>
+                    <h4>Professional Teacher</h4>
+                </div>
+                <div class="col-4">
+                    <div class="iconBulat"><i class="fa-solid fa-cart-flatbed"></i></div>
+                    <h4>Sport Field</h4>
+                </div>
+                <div class="col-4">
+                    <div class="iconBulat"><i class="fa-solid fa-cart-flatbed"></i></div>
+                    <h4>Classroom</h4>
+                </div>
+                <div class="col-4">
+                    <div class="iconBulat"><i class="fa-solid fa-cart-flatbed"></i></div>
+                    <h4>Discussions</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- tim -->
+    <div class="tim">
+        <div class="container">
+            <h3>Our Team</h3>
+            <p>Jika ada pertanyaan yang perlu diskusikan, Anda bisa langsung menghubungi salah satu Tim Kami berikut ini</p>
+            <div class="box">
+                <div class="col-3">
+                    <img src="img/Designing.jpg" class="iconBulat">
+                    <h5>Rizky Fajar</h5>
+                    <p><a href="pesantofajar.php">Kirim Pesan</a></p>
+                </div>
+                <div class="col-3">
+                    <img src="img/Designing.jpg" class="iconBulat">
+                    <h5>Syahrul Rahman</h5>
+                    <p><a href="pesantorahman.php">Kirim Pesan</a></p>
+                </div>
+                <div class="col-3">
+                    <img src="img/Designing.jpg" class="iconBulat">
+                    <h5>Muhammad Naufal</h5>
+                    <p><a href="pesantonaufal.php">Kirim Pesan</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- about -->
+    <div class="about">
+        <div class="container">
+            <h3>Informasi tentang Fajar Coding</h3>
+            <p>Fajar Coding adalah pusat terpercaya untuk belajar koding dari nol hingga mahir.</p>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <footer>
+        <div>
+            <small>Copyright &copy; 2023 - FAJAR CODING, All Rights Reserved</small>
+        </div>
+    </footer>
+</body>
+</html>
